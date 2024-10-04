5e76d757a65434c04091f9d9b2c3aa90254a25b2
NirshalNiru <nirshalniru@gmail.com>
2024-10-04 13:28:17 -0500

    code will now copy images to the dataset

--- object-detection/debug.py
+++ object-detection/debug.py
@@ -13,3 +13,11 @@ frame_files = sorted(os.listdir("../video-segmentation/video-frames"))
 print("length of frame_files", len(frame_files))
 train_split = int(len(frame_files) * 0.8)
 print("train_split", train_split)
+
+train_frames = frame_files[:train_split]
+test_frames = frame_files[train_split:]
+
+# now read these images from the video-frames folder and save them in the dataset folder
+for frame in train_frames:
+    img = cv2.imread("../video-segmentation/video-frames/" + frame)
+    cv2.imwrite("dataset/train/images/" + frame, img)
--- object-detection/object-detection.py
+++ object-detection/object-detection.py
@@ -5,4 +5,38 @@ import os
 
 class generate_dataset:
     def __init__(self):
+        #create the dataset folder
+        if not os.path.exists("dataset"):
+            os.mkdir("dataset")
+            os.mkdir("dataset/train")
+            os.mkdir("dataset/train/images")
+            os.mkdir("dataset/train/labels")
+            os.mkdir("dataset/val")
+            os.mkdir("dataset/val/images")
+            os.mkdir("dataset/val/labels")
+
+    def create_images(self):
+        frames = sorted(os.listdir("../video-segmentation/video-frames"))
+        train_frames = frames[:int(len(frames) * 0.8)]
+        val_frames = frames[int(len(frames) * 0.8):]
+
+        for frame in train_frames:
+            img = cv2.imread("../video-segmentation/video-frames/" + frame)
+            cv2.imwrite("dataset/train/images/" + frame, img)
+
+        for frame in val_frames:
+            img = cv2.imread("../video-segmentation/video-frames/" + frame)
+            cv2.imwrite("dataset/val/images/" + frame, img)
+
+    def create_labels(self):
         pass
+
+    
+    def main(self):
+        self.create_images()
+        self.create_labels()
+
+
+if __name__ == "__main__":
+    dataset = generate_dataset()
+    dataset.main()
